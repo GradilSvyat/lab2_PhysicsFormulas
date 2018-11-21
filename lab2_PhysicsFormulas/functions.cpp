5011fc15a06551438490c67f9da7a2930a56c834
GradilSvyat <svyatoslav.gradil@gmail.com>
2018-11-21 15:09:59 +0200

    NewFeatures

--- lab2_PhysicsFormulas/functions.cpp
+++ lab2_PhysicsFormulas/functions.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "functions.h"
 
 using namespace std;
 const double G = 9.80665; //константа ускорения свободного падения
@@ -10,6 +11,7 @@ void Start_print()
 	cout << "\"Давление на глубине жидкости\" \t- введите 1" << endl;
 	cout << "\"Сила тяжести\" \t\t\t- введите 2" << endl;
 	cout << "\"Ускорение\" \t\t\t- введите 3" << endl;
+	cout << "\"Кинетическая энергия тела\" \t- введите 4" << endl;
 	cout << "Завершить роботу программы \t- введите 0" << endl << endl;
 }
 double P(double p, double h) //Расчёт формулы давления жидкости
@@ -24,4 +26,8 @@ double a(double v0, double v, double t) //Расчёт формулы ускорения
 {
 	double dv = v - v0; //дельта скорости
 	return dv / t;
+}
+double Ek(double m1, double v1) //Расчёт формулы кинетической энергии тела
+{
+	return (m1*v1*v1)/2;
 }
--- lab2_PhysicsFormulas/functions.h
+++ lab2_PhysicsFormulas/functions.h
@@ -4,4 +4,5 @@ void Start_print();
 double P(double p, double h); //Расчёт формулы давления жидкости
 double F(double m); //Расчёт формулы силы тяжести
 double a(double v0, double v, double t); //Расчёт формулы ускорения
+double Ek(double m1, double v1); //Расчёт формулы кинетической энергии тела
 
